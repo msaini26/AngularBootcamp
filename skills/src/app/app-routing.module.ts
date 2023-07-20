0c42e64a74ea9eff51162efefa8ab619ef69a0fc
msaini26 <mansisaini26@gmail.com>
2023-07-20 08:47:36 -0700

    day 1 complete

--- skills/src/app/app-routing.module.ts
+++ skills/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import Home component
 import { HomeComponent } from "./home/home.component";
-
+import { RegisterComponent } from "./register/register.component";
+import { EmployeesComponent } from "./employees/employees.component";
+//
 const routes: Routes = [
-  // redirects to home and makes sure that your path is complete
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'employees', component: EmployeesComponent}
 ];
-
+//
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- skills/src/app/app.module.ts
+++ skills/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
